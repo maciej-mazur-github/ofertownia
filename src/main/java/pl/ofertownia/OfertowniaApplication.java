aec177d4f0e79647dce7701d0a0470ebdc89e2e6
Maciej Mazur <maciej.mazur.mailbox@gmail.com>
2024-08-29 00:16:38 +0200

    Final version

---
+++ src/main/java/pl/ofertownia/OfertowniaApplication.java
@@ -0,0 +1,13 @@
+package pl.ofertownia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfertowniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfertowniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ofertownia/OfertowniaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ofertownia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfertowniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
